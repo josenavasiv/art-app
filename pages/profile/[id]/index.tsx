a4fd29dff8ea1e11267eca71c086f9d265ae1799
Alexander <josenavasiv@gmail.com>
2022-04-20 00:38:45 -0700

    git commit before changing the artwork page

--- pages/profile/[id]/index.tsx
+++ pages/profile/[id]/index.tsx
@@ -39,28 +39,30 @@ const index: React.FC = ({ userDetails, userArtworks }: InferGetServerSidePropsT
 			<Navbar />
 
 			<div className="flex justify-center">
-				<div className="flex flex-col max-w-[1400px] w-full items-center justify-center p-5">
+				<div className="flex flex-col h-[320px] w-full items-center justify-center relative overflow-hidden">
 					<img className="rounded-full w-28 h-28" src={userDetails.avatar ?? userDetails.image} alt="" />
 					<div className="text-3xl font-semibold text-[#E63E6D]">
 						{userDetails.displayName ?? userDetails.name}
 					</div>
 					<div className="text-sm font-medium text-[#E63E6D]">{userDetails.headline}</div>
+					<img className="absolute -z-10 object-cover" src={userDetails?.backgroundImageUrl} alt="" />
 				</div>
 			</div>
-			<Tabs className="text-gray-300 mt-6">
+
+			<Tabs className="text-gray-300 z-10 mt-2 w-full">
 				<TabList className="font-semibold text-center">
-					<Tab>Artwork</Tab>
 					<Tab>About</Tab>
+					<Tab>Artwork</Tab>
 					<Tab>Likes</Tab>
 					<Tab>Following</Tab>
 					<Tab>Followers</Tab>
 				</TabList>
 
 				<TabPanel>
-					<ArtworkGrid artworks={userArtworks} />
+					<h2>About</h2>
 				</TabPanel>
 				<TabPanel>
-					<h2>About</h2>
+					<ArtworkGrid artworks={userArtworks} />
 				</TabPanel>
 				<TabPanel>
 					<h2>Likes</h2>
