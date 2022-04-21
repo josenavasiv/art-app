38a67bbf6838e985d77f33469422a2b04be3169b
Alexander <josenavasiv@gmail.com>
2022-04-21 14:47:21 -0700

    Provided a temporary fix for liking bug

--- pages/artwork/[id]/index.tsx
+++ pages/artwork/[id]/index.tsx
@@ -55,14 +55,13 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 			},
 		},
 	});
-	if (likeResult) {
-		userCanLike = false;
-	}
+
 	return {
 		props: {
 			artworkDetails,
 			session,
 			userCanLike,
+			likeResult,
 		},
 	};
 };
@@ -78,10 +77,12 @@ const index: React.FC = ({
 	artworkDetails,
 	session,
 	userCanLike,
+	likeResult,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
 	const router = useRouter();
+
 	const [showModal, setShowModal] = useState(false);
-	const [canLike, setCanLike] = useState(userCanLike);
+	const [canLike, setCanLike] = useState(likeResult ? false : true);
 
 	const { user, isLoading: userIsLoading, isError: userIsError } = useUser(artworkDetails.authorId);
 	const {
@@ -133,7 +134,7 @@ const index: React.FC = ({
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
 		});
-		setCanLike(false);
+		router.reload();
 	};
 
 	const handleUnlike = async () => {
@@ -141,7 +142,7 @@ const index: React.FC = ({
 			method: 'DELETE',
 			headers: { 'Content-Type': 'application/json' },
 		});
-		setCanLike(true);
+		router.reload();
 	};
 
 	return (
@@ -155,7 +156,7 @@ const index: React.FC = ({
 						<img src={artworkDetails.imageUrl} alt="" className="self-center" />
 					</div>
 
-					<div className="fixed bg-gray-900 text-white h-full w-[320px] overflow-y-auto right-0 p-5 pt-[76px] space-y-6">
+					<div className="fixed bg-gray-900 text-white h-full w-[320px] overflow-y-auto right-0 p-5 pt-[76px] space-y-4">
 						<div className="flex flex-row space-x-3 relative">
 							{userIsLoading && <div>LOADING USER DETAILS</div>}
 							{user && (
@@ -266,7 +267,25 @@ const index: React.FC = ({
 						</div>
 						{session && (
 							<div className="flex flex-row items-center">
-								{canLike ? (
+								{likeResult ? (
+									<div
+										onClick={handleUnlike}
+										className="bg-blue-300 w-1/5 rounded-sm cursor-pointer py-1 px-8 flex flex-row text-xs justify-center items-center space-x-1"
+									>
+										<div>
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												className="h-[17px] w-[15px] hover:text-white pb-0.5"
+												viewBox="0 0 20 20"
+												fill="currentColor"
+											>
+												<path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
+											</svg>
+										</div>
+
+										<div className="font-bold">LIKED</div>
+									</div>
+								) : (
 									<div
 										onClick={handleLike}
 										className="bg-blue-300 w-1/5 rounded-sm cursor-pointer py-1 px-3 flex flex-row text-xs justify-center items-center space-x-1"
@@ -289,24 +308,6 @@ const index: React.FC = ({
 										</div>
 
 										<div className="font-bold">LIKE</div>
-									</div>
-								) : (
-									<div
-										onClick={handleUnlike}
-										className="bg-blue-300 w-1/5 rounded-sm cursor-pointer py-1 px-8 flex flex-row text-xs justify-center items-center space-x-1"
-									>
-										<div>
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												className="h-[17px] w-[15px] hover:text-white pb-0.5"
-												viewBox="0 0 20 20"
-												fill="currentColor"
-											>
-												<path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
-											</svg>
-										</div>
-
-										<div className="font-bold">LIKED</div>
 									</div>
 								)}
 							</div>
